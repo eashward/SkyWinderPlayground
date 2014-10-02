2fc53564f4da2f8c9b2ca76cd27fb6f540884378
Petr Korolev <sky4winder@gmail.com>
2014-10-02 14:05:53 +0300

    add tableview example

--- SkyWinderPlayground.playground/section-2.swift
+++ SkyWinderPlayground.playground/section-2.swift
@@ -1,5 +1,34 @@
-// Playground - noun: a place where people can play
 
-import UIKit
+class DataSource: NSObject, UITableViewDataSource{
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 7;
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let row = indexPath.row
+        
+        let style = UITableViewCellStyle.fromRaw(row)
+        
+        //        let cell = UITableViewCell(style: style!, reuseIdentifier: nil)
+        let cell = UITableViewCell(style: style ?? .Default, reuseIdentifier: nil)
+        
+        cell.textLabel!.text = "text"
+        
+        if let detailtTextLabel = cell.detailTextLabel
+        {
+            detailtTextLabel.text = "Detail text"
+        }
+        
+        return cell
+    }
+}
 
-var str2 = "Hello!!!!!!"
+let ds = DataSource()
+let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 320, height: 240), style: .Plain)
+
+tableView.dataSource = ds
+tableView.reloadData()
+
+
